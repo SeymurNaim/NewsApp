316ffad69b424be0eadeb3c02ab62b7265fbd58c
SeymurNaim <naimbeyov@mail.ru>
2024-05-31 19:11:14 +0400

    updates

--- NewsApp/SceneDelegate.swift
+++ NewsApp/SceneDelegate.swift
@@ -27,7 +27,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     func mainPageRoot(windowScene: UIWindowScene) {
         window?.windowScene = windowScene
-        window?.rootViewController = TabbarVC()
+        window?.rootViewController = LoginVC()
         window?.makeKeyAndVisible()
     }
 
